8df1f627744d5ae1e1f8b85de3fc97dca209c29e
kumail86 <kumail86@gmail.com>
2015-05-04 02:35:56 -0500

    Meteorologist Complete

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -9,6 +9,7 @@ class GeocodingController < ApplicationController
   def street_to_coords
     @street_address = params[:user_street_address]
     url_safe_street_address = URI.encode(@street_address)
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{url_safe_street_address}&sensor=false"
 
     # ==========================================================================
     # Your code goes below.
@@ -17,11 +18,10 @@ class GeocodingController < ApplicationController
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
 
+    parsed_data = JSON.parse(open(url).read)
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
-
-    @latitude = "Replace this string with your answer."
-
-    @longitude = "Replace this string with your answer."
 
     render("street_to_coords.html.erb")
   end
