63602e71a89524dbd53646e7002087da2d27f335
Dave Newberry <david.newberry@gmail.com>
2015-05-05 07:54:03 -0500

    Completed final part of HW

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -18,8 +18,8 @@ class GeocodingController < ApplicationController
     # ==========================================================================
 
     parsed_data = JSON.parse(open("https://maps.googleapis.com/maps/api/geocode/json?address=" + url_safe_street_address).read)
-    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
-    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+    @lat = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @lng = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
     render("street_to_coords.html.erb")
   end
