b524e29dd952f1c2ce24b387eee8d52f4a2c1b7b
YamaMoz <m.keenoy@gmail.com>
2015-05-06 15:34:56 -0500

    Submitted week 5 homework

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -9,25 +9,26 @@ class MeteorologistController < ApplicationController
   def street_to_weather
     @street_address = params[:user_street_address]
     url_safe_street_address = URI.encode(@street_address)
+    target_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{url_safe_street_address}"
+    raw_data = open(target_url).read
+    data_hash = JSON.parse(raw_data)
+    lat = data_hash["results"][0]["geometry"]["location"]["lat"]
+    lng = data_hash["results"][0]["geometry"]["location"]["lng"]
 
-    # ==========================================================================
-    # Your code goes below.
-    # The street address the user input is in the string @street_address.
-    # A URL-safe version of the street address, with spaces and other illegal
-    #   characters removed, is in the string url_safe_street_address.
-    # ==========================================================================
+    weather_url = "https://api.forecast.io/forecast/08c3bd7e8db7fd9577933d892b66797e/#{lat},#{lng}"
+    weather_data = open(weather_url).read
+    weather_hash = JSON.parse(weather_data)
 
 
+    @current_temperature = weather_hash["currently"]["temperature"]
 
-    @current_temperature = "Replace this string with your answer."
+    @current_summary = weather_hash["currently"]["summary"]
 
-    @current_summary = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = weather_hash["minutely"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_several_hours = weather_hash["hourly"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = weather_hash["daily"]["summary"]
 
     render("street_to_weather.html.erb")
   end
